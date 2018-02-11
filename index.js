f311b9167d00361de12b0176296be8724912a571
orthagonal <orthagonal1@gmail.com>
2018-02-11 16:37:49 -0600

    pass auth into route

--- index.js
+++ index.js
@@ -11,40 +11,48 @@ const defaults = {
 const register = function(server, options) {
   const settings = Object.assign({}, defaults, options);
 
-  server.route({
-    method: 'get',
-    path: settings.endpoint,
-    async handler(request, h) {
-      if (settings.token && settings.token !== request.query.token) {
-        throw Boom.unauthorized();
+  const handler = async (request, h) => {
+    if (settings.token && settings.token !== request.query.token) {
+      throw Boom.unauthorized();
+    }
+
+    const output = {
+      host: server.info.host,
+      env: process.env.NODE_ENV,
+      uptime: Math.floor(process.uptime()),
+      cpu: process.cpuUsage(),
+      memory: process.memoryUsage()
+    };
+
+    await async.each(settings.checks, async check => {
+      if (!check.name || !check.method) {
+        throw new Boom('Invalid check');
       }
 
-      const output = {
-        host: server.info.host,
-        env: process.env.NODE_ENV,
-        uptime: Math.floor(process.uptime()),
-        cpu: process.cpuUsage(),
-        memory: process.memoryUsage()
-      };
-
-      await async.each(settings.checks, async check => {
-        if (!check.name || !check.method) {
-          throw new Boom('Invalid check');
+      output[check.name] = await str2fn.execute(
+        check.method,
+        server.methods,
+        {
+          request,
+          options: check.options
         }
+      );
+    });
 
-        output[check.name] = await str2fn.execute(
-          check.method,
-          server.methods,
-          {
-            request,
-            options: check.options
-          }
-        );
-      });
+    return output;
+  };
 
-      return output;
-    }
-  });
+  const routeConfig = {
+    method: 'get',
+    path: settings.endpoint,
+    handler
+  };
+  if (options.auth) {
+    routeConfig.config = {
+      auth: options.auth
+    };
+  }
+  server.route(routeConfig);
 };
 
 exports.plugin = {
